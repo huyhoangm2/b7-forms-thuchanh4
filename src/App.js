38670b45a3d6600e39ebbfd6a00bffe4f0ddc008
huyhoangm2 <hoangken760@gmail.com>
2023-02-28 20:15:31 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+export default function App() {
+  const [values, setValues] = useState({ email: '', name: '', content: '' });
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const handleChange = (event) => {
+    setValues({ ...values, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = (ev) => {
+    ev.preventDefault();
+    alert(JSON.stringify(values));
+  };
+return (
+  <div className="container">
+      <h1>Liên hệ</h1>
+      <form onSubmit={handleSubmit}>
+        <p>Nhập tên:</p>
+        <input
+          name="name"
+          type="text"
+          defaultValue={values.name}
+          onChange={handleChange}
+        />
+        <p>Nhập email:</p>
+        <input
+          name="email"
+          type="email"
+          defaultValue={values.email}
+          onChange={handleChange}
+        />
+        <p>Nội dung muốn gửi:</p>
+        <textarea
+          name="content"
+          defaultValue={values.content}
+          onChange={handleChange}
+        />
+        <br />
+
+        <p>bấm submit form</p>
+        <button>submit nè</button>
+      </form>
     </div>
-  );
-}
-
-export default App;
+);}
